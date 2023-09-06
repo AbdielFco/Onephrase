22e76a9f9b5f3d73f2c95eb5166b9cc65105ca42
AbdielFco <88990612+AbdielFco@users.noreply.github.com>
2023-09-06 15:50:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-// Función para obtener una frase aleatoria en español de la API "Frases de la Vida"
-async function obtenerFraseAleatoria() {
+// Función para obtener una frase aleatoria en inglés de la API "Quotable"
+async function obtenerFraseAleatoriaEnIngles() {
     try {
-        const response = await fetch('https://api.frasesvida.com/v1/random');
+        const response = await fetch('https://api.quotable.io/random');
         const data = await response.json();
-        return data.data.frase;
+        return data.content;
     } catch (error) {
         console.error('Error al obtener la frase:', error);
         return 'No se pudo obtener la frase.';
     }
 }
 
+// Función para traducir una frase de inglés a español utilizando la API de Google Cloud Translation
+async function traducirFraseAlEspanol(frase) {
+    try {
+        const response = await fetch('https://translation.googleapis.com/language/translate/v2', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                q: frase,
+                source: 'en',
+                target: 'es',
+                format: 'text',
+            }),
+        });
+        const data = await response.json();
+        return data.data.translations[0].translatedText;
+    } catch (error) {
+        console.error('Error al traducir la frase:', error);
+        return 'No se pudo traducir la frase.';
+    }
+}
+
 // Función para generar y mostrar una frase aleatoria en español
 async function generarFrase() {
-    const fraseAleatoria = await obtenerFraseAleatoria();
-    document.getElementById("frase").textContent = fraseAleatoria;
+    const fraseEnIngles = await obtenerFraseAleatoriaEnIngles();
+    const fraseEnEspanol = await traducirFraseAlEspanol(fraseEnIngles);
+    document.getElementById("frase").textContent = fraseEnEspanol;
 }
 
 // Event listener para el botón
